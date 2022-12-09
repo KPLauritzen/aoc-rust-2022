b98bfcdfd8e88aac4537f888e6fd8324b1fca291
Kasper Primdal Lauritzen <klaur@dfds.com>
2022-12-09 09:57:26 +0100

    day5 start

---
+++ src/bin/day05.rs
@@ -0,0 +1,47 @@
+// https://adventofcode.com/2022/day/5
+use std::num::ParseIntError;
+use aoc_rust_2022::{file_to_vec};
+
+
+
+fn main() {
+    let filename = "input/day05.txt";
+    let input = file_to_vec(filename).unwrap();
+    let part_1_result = part_1(&input).unwrap();
+    println!("Part 1: {}", part_1_result);
+
+    let part_2_result = part_2(&input).unwrap();
+    println!("Part 2: {}", part_2_result);
+
+}
+
+
+fn part_1(input: &Vec<String>) -> Result<i32, ParseIntError> {
+    todo!()    
+}
+
+fn part_2(input: &Vec<String>) -> Result<i32, ParseIntError> {
+    todo!()    
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    // Part 1
+    #[test]
+    fn part_1_sample_input() {
+        let filename = "input/day05_sample.txt";
+        let input = file_to_vec(filename).unwrap();
+        let result = part_1(&input).unwrap();
+        assert_eq!(result, 24000);
+    }
+
+    // Part 2
+    #[test]
+    fn part_2_sample_input() {
+        let filename = "input/day05_sample.txt";
+        let input = file_to_vec(filename).unwrap();
+        let result = part_2(&input).unwrap();
+        assert_eq!(result, 45000);
+    }
+}
